97327c6557e8afdab72bf0ba4ec4910378988251
Sam Poder <39828164+sampoder@users.noreply.github.com>
2025-02-21 19:24:18 -0800

    Add the different modes

--- main.py
+++ main.py
@@ -12,34 +12,43 @@ direction = 'South'
 
 last_played = 0
 ser.write(bytearray("BLANK",'ascii'))
-for x in range(30):
-    ser.write(bytearray("RED",'ascii'))
-    time.sleep(1)
 
-ser.write(bytearray("BLANK",'ascii'))
+mode = "STANDARD"
+# mode = "DEMO"
+# mode = "LEAVE_THE_HOUSE"
 
-playsound('leaving.mp3')
-
-while True:
+if mode == "DEMO":
+    for x in range(10):
+        ser.write(bytearray("RED",'ascii'))
+        time.sleep(1)
+    
     ser.write(bytearray("BLANK",'ascii'))
-
-while False:
-    response = requests.get(url)
-    incoming = response.json()["root"]["station"][0]["etd"]
-    flag = True
-    for line in incoming:
-      approaching = line["estimate"][0]
-      if approaching["direction"] == direction:
-        if approaching['minutes'] == 'Leaving':
-          print("byebye!")
-          if last_played + 60 < int(time.time()):
-              last_played = int(time.time())
-              playsound('leaving.mp3')
-        elif int(approaching['minutes']) < 2:
-          print(approaching["color"])
-          flag = False
-          ser.write(bytearray(approaching["color"],'ascii'))
-    if flag:
-      ser.write(bytearray("BLANK",'ascii'))
-    time.sleep(1)
-
+    
+    playsound('leaving.mp3')
+    
+    while True:
+        ser.write(bytearray("BLANK",'ascii'))
+else:
+    while True:
+        response = requests.get(url)
+        incoming = response.json()["root"]["station"][0]["etd"]
+        flag = True
+        for line in incoming:
+          approaching = line["estimate"][0]
+          if approaching["direction"] == direction:
+            if (approaching['minutes'] == 'Leaving' and mode == "STANDARD") or (approaching['minutes'] == '13' and mode == "LEAVE_THE_HOUSE"):
+                print("byebye!")
+                if last_played + 60 < int(time.time()):
+                    last_played = int(time.time())
+                    playsound('leaving.mp3')
+            elif (approaching['minutes'] != 'Leaving' and int(approaching['minutes']) < 2 and mode == "STANDARD") or (approaching['minutes'] == '14' and mode == "LEAVE_THE_HOUSE"):
+                print(approaching["color"])
+                flag = False
+                ser.write(bytearray(approaching["color"],'ascii'))
+            else:
+                print(approaching["color"])
+                print(approaching['minutes'])
+        if flag:
+          ser.write(bytearray("BLANK",'ascii'))
+        time.sleep(1)
+    
